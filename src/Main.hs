f11e9575ec769f171bca39b8295a5d24a1fa0683
Steven Shaw <steven@steshaw.org>
2015-07-07 17:38:34 +1000

    Make runnable

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,12 @@
-import QuickCheck
+import Test.QuickCheck
 
 absAverage :: [Double] -> Double
 absAverage ds = sum ds / fromIntegral (length ds)
 
-quickCheck1 :: IO ()
-quickCheck1 = quickCheck $ \x -> absAverage x >= 0
+--quickCheck1 :: IO ()
+quickCheck1 = \x -> absAverage x >= 0
 
 main :: IO ()
 main = do
   putStrLn "Hello QuickCheck"
+  quickCheck quickCheck1
